c83bfd0f1a0522c395c08369db372017813fa123
Kunjan Gevariya <kunjangevariya2468@gmail.com>
2021-11-16 21:56:34 +0530

    initial commit schedule

--- src/components/SchedulePage/SchedulePage.js
+++ src/components/SchedulePage/SchedulePage.js
@@ -1,9 +1,13 @@
 import "./SchedulePage.css"
 
-const SchedulePage=()=>{
-  return(<div className="schedule" id="schedule">
-    hi
-  </div>)
+const SchedulePage = () => {
+  return (
+    <div className="schedule" id="schedule">
+      <div className="sch_title">
+        <span className="big_s">S</span>chedule
+      </div>
+    </div>
+  )
 }
 
 export default SchedulePage;
