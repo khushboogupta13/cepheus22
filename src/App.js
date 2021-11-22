7224286eb767db1d5a187d304b9ea04f9f2df2d9
Arpit Maurya <arpitchatting@gmail.com>
2021-11-22 14:53:24 +0530

    added slider

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Navbar from "./components/Navbar/Navbar";
 import SchedulePage from "./components/SchedulePage/SchedulePage";
 import Sponsorship from "./components/Sponsorship/Sponsorship";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
--- src/components/verticalCarousal/tempFile.jsx
+++ src/components/verticalCarousal/tempFile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useCallback, CSSProperties, useEffect, useRef } from '
 import FlipCard from '../flipCards/flipCards';
 import { useInViewport } from 'react-in-viewport';
 import './styles.css';
+import Slider from 'react-slick';
 // import { Swiper, SwiperSlide } from 'swiper/react';
 // import SwiperCore, { Pagination } from 'swiper';
 // import 'swiper/swiper-bundle.min.css';
@@ -28,22 +29,24 @@ const Temp = (props) => {
 		<div className='carousalDiv'>
 			<div className='colDiv' ref={myRef}>
 				{images.map((items, j) => (
-					<div className='rowDiv' style={{ position: 'relative' }}>
+					<div className='rowDiv' style={{ zIndex: `${100 - j}` }}>
 						{/* <Swiper slidesPerView={5} spaceBetween={10}> */}
-						{items.map((item, i) => (
-							// <SwiperSlide style={{ zIndex: 10 }}>
-							<FlipCard
-								front={`${item.front}`}
-								back={`${item.back}`}
-								zIndex={100 - j}
-								order={j}
-								anime={i + j}
-								inViewport={inViewport}
-							/>
-							// </SwiperSlide>
-						))}
-						{/* </Swiper> */}
-						{/* </Slider> */}
+						<Slider {...settings}>
+							{items.map((item, i) => (
+								// <SwiperSlide style={{ zIndex: 10 }}>
+								<FlipCard
+									front={`${item.front}`}
+									back={`${item.back}`}
+									zIndex={100 - j}
+									order={j}
+									anime={i + j}
+									inViewport={inViewport}
+								/>
+								// </SwiperSlide>
+							))}
+							{/* </Swiper> */}
+							{/* </Slider> */}
+						</Slider>
 					</div>
 				))}
 			</div>
