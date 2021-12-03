f24c45bc71bcda9ce443a6fe8136245808a030db
khushboogupta13 <guptakhushi345@gmail.com>
2021-12-03 10:47:25 +0530

    fix: event page bugs (responsive)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,4 @@
 import "./App.css";
-import AboutUs from "./components/AboutUs/AboutUs";
-import ContactUs from "./components/ContactUs/ContactUs";
-import Events from "./components/Events/Events";
-import FAQ from "./components/FAQ/FAQ";
-import Home from "./components/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import SchedulePage from "./components/SchedulePage/SchedulePage";
-import Sponsorship from "./components/Sponsorship/Sponsorship";
 import IndividualEvent from "./components/IndividualEvent/IndividualEvent";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
