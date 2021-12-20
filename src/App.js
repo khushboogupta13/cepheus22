bae763ef80e2a801071b702919f44d05dc5b78cd
Sahil Verma <sahilverma817@gmail.com>
2021-12-20 11:15:12 +0530

    Loader updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import React, { useEffect, useState } from "react";
-import Login from './components/LogIn/Login'
+import Login from "./components/LogIn/Login";
 import {
   Route,
   Switch,
@@ -31,63 +31,54 @@ const App = () => {
   function headerView() {
     console.log("window location", location.pathname === "/callback/");
     if (location.pathname === "/callback/") {
-      let paramString = location.search.split('?')[1];
-      let params_arr = paramString.split('&');
+      let paramString = location.search.split("?")[1];
+      let params_arr = paramString.split("&");
       console.log(params_arr[0], params_arr[1]);
       let playerId;
       let is_profile_complete;
-      
-        let _pair = params_arr[0].split('=');
-        playerId = _pair[1];
 
-        _pair = params_arr[1].split('=');
-        is_profile_complete = _pair[1];
-        
-      
+      let _pair = params_arr[0].split("=");
+      playerId = _pair[1];
+
+      _pair = params_arr[1].split("=");
+      is_profile_complete = _pair[1];
+
       //console.log("callback  called",localStorage.getItem('id'))
       toast.success("Successfully Loged In");
-      if(playerId && is_profile_complete === 'true'){
+      if (playerId && is_profile_complete === "true") {
         history.push("/");
-      }else if(playerId && is_profile_complete === 'false'){
-            history.push(`/completeProfile/?${playerId} `);
+      } else if (playerId && is_profile_complete === "false") {
+        history.push(`/completeProfile/?${playerId} `);
       }
-      
     }
   }
   useEffect(() => {
     headerView();
   });
 
-  if (isLoaded) {
-    return (
-      <div className="App">
-        <Toaster />
-        <Switch>
-          <Route path="/" exact>
-            <SiteContainer />
-          </Route>
-          <Route path="/completeProfile" exact>
-            <Login />
-          </Route>
-          <Route path="/event/:eventName" exact>
-            <IndividualEvent />
-          </Route>
-          <Route path="/workshops/:eventName" exact>
-            <IndividualEvent />
-          </Route>
-          <Route path="/talks/:eventName" exact>
-            <IndividualEvent />
-          </Route>
-        </Switch>
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <Loader />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      {!isLoaded && <Loader />}
+      <Toaster />
+      <Switch>
+        <Route path="/" exact>
+          <SiteContainer />
+        </Route>
+        <Route path="/completeProfile" exact>
+          <Login />
+        </Route>
+        <Route path="/event/:eventName" exact>
+          <IndividualEvent />
+        </Route>
+        <Route path="/workshops/:eventName" exact>
+          <IndividualEvent />
+        </Route>
+        <Route path="/talks/:eventName" exact>
+          <IndividualEvent />
+        </Route>
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
