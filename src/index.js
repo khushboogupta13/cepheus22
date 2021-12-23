81dcc07992e7cb8eaed0adc5d6bedab3bfa7c796
Arpit Maurya <arpitchatting@gmail.com>
2021-12-23 21:37:39 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
+	<HttpsRedirect>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
+	</HttpsRedirect>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
