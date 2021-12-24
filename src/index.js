96458422864792fc6835738b2b612ef8a5e35f68
agasheadwait <agasheadwait24@gmail.com>
2021-12-24 15:44:55 +0530

    Added hashrouter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import HttpsRedirect from 'react-https-redirect';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import HttpsRedirect from "react-https-redirect";
 
 ReactDOM.render(
-	<HttpsRedirect>
-		<BrowserRouter basename={process.env.PUBLIC_URL}>
-			<React.StrictMode>
-				<App />
-			</React.StrictMode>
-		</BrowserRouter>
-	</HttpsRedirect>,
-	document.getElementById('root')
+  <HttpsRedirect>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </HashRouter>
+  </HttpsRedirect>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
